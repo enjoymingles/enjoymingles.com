bf298d46015075742c04c689b68b46248aa43486
nayoonhwang <nayoon.hwang@sk.com>
2020-06-29 22:48:22 +0900

    initial commit

---
+++ src/events.js
@@ -0,0 +1,13 @@
+import moment from "moment";
+
+const events = [
+    {
+        start: moment("20200703"),
+        end: moment("20200703"),
+        title: "비어퐁&보드게임 빠띠",
+        description: "불금 야외 비어퐁과 보드게임 빠띠 놀쟝",
+        color: "#3CAEA3"
+    }
+]
+
+export default events;
