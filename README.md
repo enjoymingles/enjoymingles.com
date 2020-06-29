8d4525fd61d61af7eaf7e7fdfa16ddba1780b002
nayoonhwang <nayoon.hwang@sk.com>
2020-06-29 22:53:50 +0900

    fix Readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # React Big Calendar
 
-A plain ole' react-big-calendar.
+## Event 등록하는법
+develop 브랜치에서 src/events.js 를 양식에 맞게 수정
+
+`npm run deploy` 수행
--- src/events.js
+++ src/events.js
@@ -3,7 +3,7 @@ import moment from "moment";
 const events = [
     {
         start: moment("20200703"),
-        end: moment("20200703"),
+        end: moment("20200704"),
         title: "비어퐁&보드게임 빠띠",
         description: "불금 야외 비어퐁과 보드게임 빠띠 놀쟝",
         color: "#3CAEA3"
