bbdc41bdd21e39f0f061cac56f4ee45be58b51a8
nayoonhwang <nayoon.hwang@sk.com>
2020-06-29 22:58:28 +0900

    events

--- src/events.js
+++ src/events.js
@@ -7,6 +7,12 @@ const events = [
         title: "비어퐁&보드게임 빠띠",
         description: "불금 야외 비어퐁과 보드게임 빠띠 놀쟝",
         color: "#3CAEA3"
+    },
+    {
+        start: moment("20200705"),
+        end: moment("20200705"),
+        title: "flutter 스터디",
+        description: "flutter 스터디"
     }
 ]
 
